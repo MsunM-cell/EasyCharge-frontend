8046b4ea65d0baddd047be019686b1d532772575
Spider <2464831035@qq.com>
2022-05-29 14:06:09 +0800

    feat: bottom navigation bar of customer

---
+++ pages/customer/charge/home/home.js
@@ -0,0 +1,21 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+
+  data: {},
+
+  lifetimes: {
+    attached: function () {
+      // 在组件实例进入页面节点树时执行
+      
+    },
+
+    detached: function () {
+      // 在组件实例被从页面节点树移除时执行
+
+    }
+  },
+
+  methods: {}
+})
